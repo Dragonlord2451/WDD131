f1bb3adf28bc5107d81a3e6fd8b3919ba731d379
DragonLord <olivecollin2451@gmail.com>
2025-05-08 03:19:17 -0600

    Cool pics

--- dom basics.js
+++ dom basics.js
@@ -1,3 +1,18 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
+// Step 4: Create a new section
+const newSection = document.createElement("section");
+
+// Create and add an h2 element
+const newHeading = document.createElement("h2");
+newHeading.innerText = "DOM Basics";
+newSection.appendChild(newHeading);
+
+// Create and add a paragraph
+const newPara = document.createElement("p");
+newPara.innerText = "This was added through Javascript";
+newSection.appendChild(newPara);
+
+// Add the new section to the body
+document.body.appendChild(newSection);
